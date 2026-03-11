79ec57415922ebccdabf52900633a3e620bb3471
Jason <z5481421@ad.unsw.edu.au>
2026-03-11 17:43:34 +1100

    SAAS-80: Added delete item query and function

---
+++ src/application/item_application.ts
@@ -0,0 +1,126 @@
+import {
+  jsonHelper,
+  authHelper,
+  revokeRefreshToken,
+  AuthReq
+} from "../utils/jwt_helpers";
+import {
+  verifyRefreshToken,
+} from "../utils/jwt_config";
+import {
+  getAllItems,
+  getItemsUser,
+  updateItemQuery,
+  deleteItemFromId
+} from "../database/queries/item_queries";
+
+// look into responses
+export const getItems = authHelper(
+  async (req: AuthReq): Promise<Response> => {
+    try {
+      const body = await req.json();
+      const refreshToken = body.refreshToken;
+
+      if (refreshToken) {
+        const token = await verifyRefreshToken(refreshToken);
+        await revokeRefreshToken(token.jwt_id as string);
+      } else {
+        return jsonHelper({
+          message: "No refresh token passed in"
+        }, 400);
+      }
+     
+      let items = null;
+
+      if (!body.userId) { // want all items
+        items = await getAllItems();
+      } else {
+        items = await getItemsUser(
+          body.userId
+        );
+      }
+
+      if (items === null) {
+        return jsonHelper({
+          message: "No items found"
+        }, 404);
+      }
+
+      return jsonHelper({
+        message: "Items found",
+        items: items
+      });
+    } catch (error) {
+      return jsonHelper({
+        message: "Items fetch failed",
+        error: error
+      }, 500);
+    }
+  }
+);
+
+// update item - need to extract the item id from the query parameter
+export const updateItem = authHelper(
+  async(req: AuthReq, res: Response): Promise<Response> => {
+    try {
+      const body = await req.json();
+
+      if (!body.item_id) {
+        return jsonHelper({
+          message: "No item ID provided"
+        }, 400);
+      }
+
+      // need to have at least 1 field
+      if (Object.keys(body).length < 2) {
+        return jsonHelper({
+          message: "No items provided"
+        });
+      }
+
+      // map each field into null if undefined
+      const response = await updateItemQuery(
+        body.itemId,
+        body.sellerId ?? null,
+        body.itemName ?? null,
+        body.description ?? null,
+        body.price ?? null,
+        body.quantity_available ?? null,
+        body.image_url ?? null,
+      );
+
+      return res.status(200).json(response);
+    } catch (error) {
+      return jsonHelper({
+        message: "Update item failed"
+      }, 500);
+    }
+  }
+);
+
+// delete item
+export const deleteItem = authHelper(
+  async(req: AuthReq, res: Response): Promise<Response> => {
+    try {
+      // need to extract the item id from the param not the body
+      const body = await req.json();
+
+      if (!body.itemId) {
+        return jsonHelper({
+          message: "Item ID missing"
+        }, 400);
+      }
+
+      const response = await deleteItemFromId(body.itemId);
+
+      return jsonHelper({
+        message: "Item deleted",
+        response: response
+      });
+    } catch (error) {
+      return jsonHelper({
+        message: "Deleting item failed"
+      }, 500);
+    }
+  }
+);
--- src/database/queries/item_queries.ts
+++ src/database/queries/item_queries.ts
@@ -1,4 +1,5 @@
 import pg from "../../utils/db";
+import { jsonHelper } from "../../utils/jwt_helpers";
 
 /**
  * Fetches an itemID based on its name.
@@ -6,7 +7,7 @@ import pg from "../../utils/db";
 export async function getItemIdByName(
   itemName: string,
 ): Promise<string | null> {
-  const result = await pg`
+  const result = await sql`
     SELECT item_id 
     FROM items 
     WHERE item_name = ${itemName}
@@ -19,3 +20,81 @@ export async function getItemIdByName(
 
   return result[0].id;
 }
+
+
+/*
+ * Gets all items
+*/
+export async function getAllItems() {
+  try {
+    const items = await pg`select * from items`;
+
+    return jsonHelper({ message: "Fetch all items succeeded", payload: items });
+  } catch (error) {
+    return jsonHelper({ message: "Fetch failed", error: error}, 500);
+  }
+}
+
+/*
+ * Gets all items given a userId
+*/
+export async function getItemsUser(
+  userId: string
+) {
+  try {
+    const items = await pg`select * from items where user_id = ${userId}`;
+
+    return jsonHelper({ message: "Fetch items succeeded", payload: items});
+  } catch (error) {
+    return jsonHelper({ message: "Fetch failed", error: error}, 500);
+  }
+}
+
+/*
+ * Updates an item according to provided inputs 
+*/
+export async function updateItemQuery(
+  itemId: string,
+  sellerId: string | null,
+  itemName: string | null,
+  description: string | null,
+  price: number | null,
+  quantity_available: number | null,
+  image_url: string | null,
+): Promise<Response> {
+  try {
+    const response = await pg`
+    update items
+    set
+      seller_id = coalesce(${sellerId}, seller_id),
+      item_name = coalesce(${itemName}, item_name),
+      description = coalesce(${description}, description),
+      price = coalesce(${price}, price).
+      quantity_available = coalesce(${quantity_available}, quantity_available),
+      image_url = coalesce(${image_url}, image_url)
+      last_updated = ${new Date().toISOString()}
+    where id = ${itemId}
+    returning *
+    `;
+
+    return jsonHelper({ message: "Item updated", response : response});
+  } catch (error) {
+    return jsonHelper({ message: "Update failed", error: error }, 500);
+  }
+}
+
+/*
+ * Deletes an item given an item id
+*/
+export async function deleteItemFromId(
+  itemId: string
+) {
+  try {
+    const response = await pg`delete from items where item_id = ${itemId}`;
+
+    return response;
+  } catch (error) {
+    return jsonHelper({ error: error }, 500);
+  }
+}
+
