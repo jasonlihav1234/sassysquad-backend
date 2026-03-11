2891a82b92abce86360b649e91d94186fcd24c01
Uday Saihgal <z5507339@ad.unsw.edu.au>
2026-03-11 11:01:00 +1100

    Implemented request handler for buyer route

---
+++ src/routes/user_routes.ts
@@ -0,0 +1,10 @@
+import { getUserPurchases } from "../application/user_application";
+
+export async function handleUserRoutes(req: any, res: any) {
+  const { method, url } = req;
+
+  if (method === "GET" && url.match(/^\/users\/[^/]+\/purchases$/)) {
+    await getUserPurchases(req);
+    return;
+  }
+}
