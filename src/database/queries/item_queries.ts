e744bd84e8244cf6fecdc8e82554d2a343f46ce3
Jason <z5481421@ad.unsw.edu.au>
2026-03-11 15:05:15 +1100

    SAAS-82: Added queries for items

--- src/database/queries/item_queries.ts
+++ src/database/queries/item_queries.ts
@@ -1,4 +1,6 @@
 import { sql } from "../client";
+import pg from "../../utils/db";
+import { jsonHelper } from "../../utils/jwt_helpers";
 
 /**
  * Fetches an itemID based on its name.
@@ -18,4 +20,33 @@ export async function getItemIdByName(
   }
 
   return result[0].id;
+}
+
+
+/*
+ * Gets all items
+*/
+export async function getAllItems() {
+  try {
+    const items = await pg`select * from items`;
+
+    return jsonHelper({ message: "Fetch all items succeeded", payload: items });
+  } catch (error) {
+    return jsonHelper({ message: "Fetch failed", error: error}, 500);
+  }
+}
+
+/*
+ * Gets all items given a userId
+*/
+export async function getItemsUser(
+  userId: string
+) {
+  try {
+    const items = await pg`select * from items where user_id = ${userId}`;
+
+    return jsonHelper({ message: "Fetch items succeeded", payload: items});
+  } catch (error) {
+    return jsonHelper({ message: "Fetch failed", error: error}, 500);
+  }
 }
