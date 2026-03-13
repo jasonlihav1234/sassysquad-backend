96ba70a366627419d55036eee011e249c3511d0a
Jason Li <z5481421@ad.unsw.edu.au>
2026-03-14 00:54:48 +1100

    SAAS-106: Added deleting user query and function

--- src/application/user_application.ts
+++ src/application/user_application.ts
@@ -26,6 +26,7 @@ import {
   getUserById,
   getUserSellerOrders,
   isUserIdValid,
+  removeUserById,
 } from "../database/queries/user_queries";
 import { VercelRequest } from "@vercel/node";
 
@@ -551,6 +552,28 @@ export const getMyProfileDetails = authHelper(
       return jsonHelper(
         {
           message: "Cannot get user details",
+          error: error,
+        },
+        500,
+      );
+    }
+  },
+);
+
+export const deleteUser = authHelper(
+  async (req: AuthReq): Promise<Response> => {
+    try {
+      const response = await removeUserById(req.user?.subject_claim as string);
+
+      return jsonHelper({
+        message: "User successfully deleted",
+        response: response,
+      });
+    } catch (error) {
+      console.log(error);
+      return jsonHelper(
+        {
+          message: "Failed to delete user",
           error: error,
         },
         500,
