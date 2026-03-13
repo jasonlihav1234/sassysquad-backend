f2fbcaaf8b0cf73c2976ce203be6e074594a0eb8
Jason Li <166970548+jasonlihav1234@users.noreply.github.com>
2026-03-13 14:36:13 +1100

    SAAS-76: Creating swagger file (#24)
    * saas-76: Adding swagger files
    
    * SAAS-76: Removing bloat files
    
    ---------
    
    Co-authored-by: Jason Li <z5481421@ad.unsw.edu.au>

---
+++ api-docs/swagger-initializer.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: [
+      {
+        url: "../swagger.json",
+        name: "Local file test"
+      },
+      {
+        url: "http://random-route/auth/login",
+        name: "Test login route"
+      }
+    ],
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
