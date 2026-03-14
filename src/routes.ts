eaec3dd62bd0112b66d2d4ffac35447e0e6a0969
uKiranN <z5691211@ad.unsw.edu.au>
2026-03-14 23:31:21 +1100

    SAAS-57: Implemented function of PUT/orders (#22)

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import { create } from "xmlbuilder2";
-import { createOrderQuery } from "./database/queries/order_queries";
+import { createOrderQuery, getOrderById, updateOrdersById } from "./database/queries/order_queries";
 import {
   register,
   login,
@@ -389,6 +389,34 @@ export async function handleRequest(req: any, res: any) {
 
     const responseBody = await response.json();
     return res.status(response.status).json(responseBody);
+  }
+
+  // PUT /orders
+  if (method === "PUT" && /\/orders\/[^/]+/.test(url)) {
+    const { userId, updates } = body || {};
+    const orderId = url.split("/")[1];
+
+    if (!orderId) {
+      return res.status(400).json({ error: "Bad Request" });
+    }
+
+    // if () { // TODO: invalid access token
+    //   return res.status(401).json({ error: "Unauthorised" });
+    // }
+
+    const order = await getOrderById(orderId);
+
+    if (userId !== order.buyerId) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+
+    if (!order) {
+      return res.status(404).json({ error: "Order not found!" });
+    }
+
+    return await updateOrdersById(orderId, updates);
+
+    return res.status(200);
   }
 
   if (url === "/profile" && method === "PATCH") {
