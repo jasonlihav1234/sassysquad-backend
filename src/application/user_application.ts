9ba5abff1bf6cf8f6dba2197adcf5eae7b5d44a3
Jason Li <z5481421@ad.unsw.edu.au>
2026-03-14 16:25:25 +1100

    SAAS-108: Moved update function to users

--- src/application/user_application.ts
+++ src/application/user_application.ts
@@ -27,6 +27,7 @@ import {
   getUserSellerOrders,
   isUserIdValid,
   removeUserById,
+  updateProfileQuery,
 } from "../database/queries/user_queries";
 import { VercelRequest } from "@vercel/node";
 
@@ -576,6 +577,39 @@ export const deleteUser = authHelper(
           message: "Failed to delete user",
           error: error,
         },
+        500,
+      );
+    }
+  },
+);
+
+export const updateProfile = authHelper(
+  async (req: AuthReq): Promise<Response> => {
+    try {
+      const userId = req.user?.subject_claim as string;
+      const body = req.body;
+
+      if (!body.username && !body.email && !body.password) {
+        return jsonHelper(
+          {
+            message: "No fields to update for the user",
+          },
+          400,
+        );
+      }
+
+      await updateProfileQuery(userId, {
+        user_name: body.username,
+        email: body.email,
+        password: body.password,
+      });
+
+      return jsonHelper({
+        message: "Details successfully updated",
+      });
+    } catch (error) {
+      return jsonHelper(
+        { message: "Profile failed to update", error: error },
         500,
       );
     }
