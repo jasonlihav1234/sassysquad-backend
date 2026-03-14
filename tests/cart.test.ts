a4c2ef4dc5e9f609baf0ab515c4ad4ded3c744af
Jason Li <z5481421@ad.unsw.edu.au>
2026-03-14 18:31:32 +1100

    SAAS-111: Adding successful delete cart item tests

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -215,3 +215,111 @@ describe("Add items to cart tests", () => {
     expect(getBody.message).toBe("Item failed to add to cart");
   });
 });
+
+describe("Deleting item from carts tests", () => {
+  beforeAll(async () => {
+    await pg`delete from items where item_id in (${itemId1})`;
+    await pg`delete from refresh_tokens where user_id in (select user_id from users where email in ('jasonli1234@gmail.com', 'jasonli8909@gmail.com'))`;
+    await pg`delete from users where email in ('jasonli1234@gmail.com', 'jasonli8909@gmail.com')`;
+
+    const registerReq = generateRequest(
+      "http://localhost/auth/register",
+      "POST",
+      {
+        email: "jasonli1234@gmail.com",
+        username: "test",
+        password: "testing123",
+      },
+    );
+
+    const registerReq2 = generateRequest(
+      "http://localhost/auth/register",
+      "POST",
+      {
+        email: "jasonli8909@gmail.com",
+        username: "test2",
+        password: "testing123",
+      },
+    );
+
+    const userRes = await register(registerReq);
+    const userRes2 = await register(registerReq2);
+    sellerId = (await userRes.json()).user;
+    sellerId2 = (await userRes2.json()).user;
+
+    await redis.del(`cart:${sellerId}`);
+
+    await pg`
+    insert into items
+    (item_id, seller_id, item_name, price, quantity_available)
+    values
+    (${itemId1}, ${sellerId2}, ${"test_item"}, ${9.5}, ${20})
+    `;
+
+    if (sellerId !== null) {
+      await redis.hset(`cart:${sellerId}`, itemId1, (100).toString());
+    }
+  });
+
+  afterAll(async () => {
+    await pg`delete from items where item_id in (${itemId1})`;
+    await pg`delete from refresh_tokens where user_id in (select user_id from users where email in ('jasonli1234@gmail.com', 'jasonli8909@gmail.com'))`;
+    await pg`delete from users where email in ('jasonli1234@gmail.com', 'jasonli8909@gmail.com')`;
+    await redis.del(`cart:${sellerId}`);
+  });
+
+  test("Successfully deletes all items", async () => {
+    await redis.hset(`cart:${sellerId}`, itemId2, (300).toString());
+    const request = generateRequest("http://localhost/auth/login", "POST", {
+      email: "jasonli1234@gmail.com",
+      password: "testing123",
+    });
+    const loginReq = await login(request);
+    const accessToken = (await loginReq.json()).accessToken;
+
+    const request2 = generateAuthenticatedRequest(
+      `/cart`,
+      "DELETE",
+      {},
+      accessToken,
+    );
+
+    const getResponse = await deleteItemFromCart(request2);
+    const getBody = await getResponse.json();
+
+    expect(getResponse.status).toBe(200);
+    expect(getBody.message).toBe("Item/s successfully removed from cart");
+
+    const res = await redis.hkeys(`cart:${sellerId}`);
+    expect(res.length).toBe(0);
+  });
+
+  test("Successfully deletes a singular item", async () => {
+    await redis.hset(`cart:${sellerId}`, itemId2, (300).toString());
+    const request = generateRequest("http://localhost/auth/login", "POST", {
+      email: "jasonli1234@gmail.com",
+      password: "testing123",
+    });
+    const loginReq = await login(request);
+    const accessToken = (await loginReq.json()).accessToken;
+
+    const request2 = generateAuthenticatedRequest(
+      `/cart/items/${itemId2}`,
+      "DELETE",
+      {
+        itemId: ":adhaanwd",
+        quantity: 1,
+      },
+      accessToken,
+    );
+
+    const getResponse = await deleteItemFromCart(request2);
+    const getBody = await getResponse.json();
+
+    expect(getResponse.status).toBe(200);
+    expect(getBody.message).toBe("Item/s successfully removed from cart");
+
+    const res = await redis.hkeys(`cart:${sellerId}`);
+    expect(res.length).not.toBe(0);
+  });
+});
