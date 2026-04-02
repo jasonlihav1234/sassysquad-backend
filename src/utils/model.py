78692040444c2fd4297ee8c02461323da2470f2c
Jason Li <z5481421@ad.unsw.edu.au>
2026-04-02 17:09:27 +1100

    SAAS-199: Fixing query

--- src/utils/model.py
+++ src/utils/model.py
@@ -15,17 +15,17 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 def fetch_items():
   query = """
   select
-    ol.price,
-    array_remove(array_agg(distinct t.name), null) as tags,
+    ol.price_at_purchase,
+    array_remove(array_agg(distinct t.tag_name), null) as tags,
     sum(ol.quantity) as volume
   from
     order_lines ol
   left join
     item_tags it on ol.item_id = it.item_id
   left join
-    tags t on it.tag_id = t.id
+    tags t on it.tag_id = t.tag_id
   group by
-    ol.item_id, ol.price
+    ol.item_id, ol.price_at_purchase
   """
 
   with psycopg2.connect(DATABASE_URL) as conn:
