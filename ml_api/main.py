5478b28a8acd719a6ed1e077094084cfc00efe4a
Jason Li <166970548+jasonlihav1234@users.noreply.github.com>
2026-04-11 16:52:18 +1000

    SAAS-222: Tuning ml model (#98)
    * SAAS-221: Adding cors middleware
    
    * SAAS-221: Fixing typo
    
    * SAAS-221: Fixing typo in allow headers
    
    * SAAS-222: Making names lowerase to conform with database style
    
    * SAAS-222: Adding saving max prices for categories
    
    * SAAS-222: Adding v2 routes for training model
    
    * SAAS-222: Adding v2 function for loading data
    
    * SAAS-222: Adding v2 estimate function
    
    * SAAS-222: Writing confidence checks for prediciton
    
    * SAAS-222: Adding prediction logic to v2 function
    
    * SAAS-222: Adding early return for insufficient data
    
    * SAAS-222: Adding v2 training model method
    
    * SAAS-222: Editing v2/predict to use new function
    
    ---------
    
    Co-authored-by: Jason Li <z5481421@ad.unsw.edu.au>

--- ml_api/main.py
+++ ml_api/main.py
@@ -34,6 +34,16 @@ def predict_market(req: EstimateRequest):
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
   
+@app.post("/v2/predict")
+def v2_predict_market(req: EstimateRequest):
+  if not predictor.trained:
+    raise HTTPException(status_code=400, detail="Model not yet trained")
+  try:
+    result = predictor.v2_estimate_market(req.tags, req.category)
+    return result
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
+  
 @app.post("/train")
 def train_model():
   try:
@@ -41,6 +51,17 @@ def train_model():
     predictor.save(path="./models/")
     return {
       "status": "Retrained and saved model"
+    }
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/v2/train")
+def v2_train_model():
+  try:
+    predictor.v2_train_model()
+    predictor.save(path="./models/")
+    return {
+      "status": "Retrained and saved v2 model"
     }
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
